ca93d170226687e24bd72e6fd83de034bc927576
jkoper <01131681@pw.edu.pl>
2022-06-30 13:32:04 +0200

    dobre wykresy psi i psi^2, dwa parametry nx i ny

--- two_dimensional_garnek_JK_AP.py
+++ two_dimensional_garnek_JK_AP.py
@@ -14,50 +14,52 @@ from mpl_toolkits import mplot3d
 from IPython.display import HTML
 sp.init_printing()
 
-n=5
-L = 1** -10 
+nx=5
+ny=2
+L = 1** -10   # w tej wersji zakładamy, że Lx=Ly
 x,y = np. linspace(0,L, 200), np. linspace(0,L,200)
 A=(2/L)**0.5
 
 def psi0(a,b):
-    psi0=(  np.sin(( n * np.pi * a)/L) * np.sin((n * np.pi * b)/L))
+    psi0=(  np.sin(( nx * np.pi * a)/L) * np.sin((ny * np.pi * b)/L))
     return psi0
 
 def psi(a,b):
-    psi=(  np.sin(( n * np.pi * a)/L) * np.sin((n * np.pi * b)/L))**2
+    psi=(  np.sin(( nx * np.pi * a)/L) * np.sin((ny * np.pi * b)/L))**2
     return psi
 
 X,Y = np.meshgrid(x,y)
 
-#psi1 = np.array([psi(x,y) for x,y in zip(np.ravel(X),np.ravel(Y))])
+psi1 = np.array([psi(x,y) for x,y in zip(np.ravel(X),np.ravel(Y))])
 #
-#PSI = psi1.reshape(X.shape)  # wiecej wartosci!
+PSI = psi1.reshape(X.shape)  # wiecej wartosci!
 
 
 ###
-#fig = plt.figure()
+fig = plt.figure()
 
-#ax=fig.add_subplot(111, projection = '3d')
-#ax.plot_surface(X,Y,PSI, cmap = 'winter')
+ax=fig.add_subplot(111, projection = '3d')
+ax.plot_surface(X,Y,PSI, cmap = 'autumn')
 
-#plt.xlabel('X')
-#plt.ylabel('Y')
-#ax.set_zlabel('Z')
-#plt.title(['Gestosc prawdopodobienstwa dla n=%d' % (n)], color= 'red')
-#plt.savefig( ['Gestosc prawdopodobienstwa n=%d.png' % (n)], transparent = True)
+plt.xlabel('X')
+plt.ylabel('Y')
+ax.set_zlabel('Z')
+plt.title(['Gestosc prawdopodobienstwa dla Nx=%d i Ny=%d' % (nx,ny)], color= 'red')
+plt.savefig( ['Gestosc prawdopodobienstwa Nx=%d i Ny=%d.png' % (nx,ny)], transparent = True)
 
 ####
+
 psi_0 = np.array([psi0(x,y) for x,y in zip(np.ravel(X),np.ravel(Y))])
 
 PSI_0 = psi_0.reshape(X.shape)  # wiecej wartosci!
 
 fig2 = plt.figure()
 
-ax=fig2.add_subplot(111, projection = '3d')
-ax.plot_surface(X,Y,PSI_0, cmap = 'winter')
+ax2=fig2.add_subplot(111, projection = '3d')
+ax2.plot_surface(X,Y,PSI_0, cmap = 'terrain')
 
 plt.xlabel('X')
 plt.ylabel('Y')
-ax.set_zlabel('Z')
-plt.title(['Funkcja falowa dla n=%d' % (n)], color= 'blue')
-plt.savefig( ['Funkcja falowa dla n=%d' % (n)], transparent = True)
+ax2.set_zlabel('Z')
+plt.title(['Funkcja falowa dla Nx=%d i Ny=%d' % (nx,ny)], color= 'blue')
+plt.savefig( ['Funkcja falowa dla Nx=%d i Ny=%d' % (nx,ny)], transparent = True)
