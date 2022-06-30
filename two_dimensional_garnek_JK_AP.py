8ce9520cc68fa2ae3ffcbb36aeca6f9a4f3efa88
jkoper <01131681@pw.edu.pl>
2022-06-30 13:00:46 +0200

    poprawny wykres kwadratu modulu funkcji

--- two_dimensional_garnek_JK_AP.py
+++ two_dimensional_garnek_JK_AP.py
@@ -5,18 +5,40 @@ Created on Wed Jun 29 18:26:01 2022
 @author: justyna koper adam piwonski
 """
 
-import numpy as np
 import sympy as sp
+from sympy.utilities.lambdify import lambdify
+import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import quad
 from matplotlib.animation import FuncAnimation
+from mpl_toolkits import mplot3d
 from IPython.display import HTML
 sp.init_printing()
 
-sx = sp.Symbol('x', real = True)
-sk = sp.Symbol('k', real = True, positive = True)
-sa = sp.Symbol('a', real = True, positive = True)
-sA = sp.Symbol('A', real = True, positive = True)
-sn = sp.Symbol('n', integer = True, positive = True)
-spsi = sp.Function('psi')
-sx, sk, spsi(sx), sa
+n=5
+L = 1** -10 
+x,y = np. linspace(0,L, 200), np. linspace(0,L,200)
+A=(2/L)**0.5
+
+
+def psi(a,b):
+    psi=(  np.sin(( n * np.pi * a)/L) * np.sin((n * np.pi * b)/L))**2
+    return psi
+
+X,Y = np.meshgrid(x,y)
+
+psi1 = np.array([psi(x,y) for x,y in zip(np.ravel(X),np.ravel(Y))])
+
+PSI = psi1.reshape(X.shape)  # wiecej wartosci!
+
+
+###
+fig = plt.figure()
+
+ax=fig.add_subplot(111, projection = '3d')
+ax.plot_surface(X,Y,PSI, cmap = 'winter')
+
+plt.xlabel('X')
+plt.ylabel('Y')
+ax.set_zlabel('Z')
+plt.title(['Gestosc prawdopodobienstwa dla n=%d' % (n)], color= 'red')
+plt.savefig( ['Gestosc prawdopodobienstwa n=%d.png' % (n)], transparent = True)
